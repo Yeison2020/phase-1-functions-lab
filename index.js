a0adb4a90088a2a34d54ebf854b8e6b4b9e20fd7
Yeison <enmanuelquiterios@gmail.com>
2021-09-21 10:58:21 -0400

    commit tree daad6501870e6b1bbfd725f9fbaffcc54d9dc331

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+  if (number > 42) {
+    return number - 42;
+  } else if (number < 42) {
+    return 42 - number;
+  }
+}
+
+function distanceFromHqInFeet(number1) {
+  if (number1 > 42) {
+    return (number1 - 42) * 264;
+  } else if (number1 < 42) {
+    return (42 - number1) * 264;
+  }
+}
+
+function distanceTravelledInFeet(distance, destination) {
+  if (distance < destination) {
+    let result = distance - destination;
+    return Math.abs(result) * 264;
+  }
+  if (destination < distance) {
+    let positive = Math.abs(distance - destination);
+    return positive * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  if (start > destination || start < destination) {
+    if (Math.abs((start - destination) * 264) < 400) {
+      return 0;
+    }
+  }
+  //----------Middle One
+
+  //----------One before the last
+  if (
+    Math.abs(destination - start * 264) > 2000 ||
+    Math.abs(start - destination * 264) < 2500
+  ) {
+    return 25;
+  }
+  //--------------------------------Last One
+  if (start > destination) {
+    let result = start - destination;
+    if (result * 264 > 2500) {
+      return "cannot travel that far";
+    }
+  }
+
+  if (start < destination) {
+    let result = destination - start;
+    if (result * 264 > 2500) {
+      return "cannot travel that far";
+    }
+  }
+}
